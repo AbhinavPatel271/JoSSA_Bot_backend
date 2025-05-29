45eae445a9c19480f766fd90b790a78d535de7ba
yash <thevoiceofyashbhamare@gmail.com>
2025-05-30 03:32:42 +0400

    Added support for ORCR Tool for first output. Created tool info in history. Changed model type

---
+++ llm_chat_agent_v2.py
@@ -0,0 +1,179 @@
+from dotenv import load_dotenv
+from agent_tools.josaa_rag_tool import rag_tool_schema, rag_pipeline
+from agent_tools.search_tool import web_search_tool_schema, web_search
+from agent_tools.ORCR_finder_rank import find_colleges_in_rank_range, college_rank_range_schema
+from groqAPIcall import get_response
+from typing import List, Dict
+import json
+
+load_dotenv()
+
+from typing import List, Dict
+
+
+def format_messages_param(chat_history: List[Dict[str, str]], user_question: str) -> List[Dict[str, str]]:
+    # Base system message defining assistant behavior
+    system_message = {
+        "role": "system",
+        "content": (
+            """You are an expert recommender. 
+            Recommend the best college to students based on the details they provide. 
+            If you are unsure about any response USE THE TOOLS provided to answer the question to the best of
+            your abilities. Provide sources whenever HREF for a given result is provided. DONOT MAKE UP SOURCES. 
+            The source must be the exact HREF"""
+        )
+    }
+    # Base user message to let LLM understand the context of the first response(which is through database)
+    base_user_msg = {
+        "role": "user",
+        "content": "Suggest me top colleges based on my rank , category and gender."
+    }
+
+    formatted_messages = [system_message]
+    formatted_messages.append(base_user_msg)
+
+    if chat_history:
+        for msg in chat_history:
+
+            if msg.get("role") in {"user", "assistant"} and "content" in msg:
+                formatted_messages.append({"role": msg["role"], "content": msg["content"]})
+
+    formatted_messages.append({"role": "user", "content": user_question})
+
+    return formatted_messages
+
+
+async def chat_agent(user_question: str, chat_history: list[dict] = None) -> dict:
+    try:
+
+        # user chat_history for assistant and user role messages
+        messages = format_messages_param(chat_history, user_question)
+
+        response = await get_response(
+            "meta-llama/llama-4-scout-17b-16e-instruct",
+            messages = messages,
+            tools=[
+                rag_tool_schema,
+                web_search_tool_schema,
+                college_rank_range_schema
+            ],
+
+        )
+
+        msg = response.choices[0].message
+        print("tool_calls:", msg.tool_calls)
+        output = None
+
+        if msg.tool_calls:
+            for tool_call in msg.tool_calls:
+                if tool_call.function.name == "rag_pipeline":
+                    try:
+                        args = json.loads(tool_call.function.arguments)
+                        result = rag_pipeline(**args)
+                        print("Result of rag tool : - ", result)
+                        if result["success"]:
+                            output = result["answer"]["result"]
+                            messages.append(
+                                {
+                                    "tool_call_id": tool_call.id,
+                                    "role": "tool",  # Indicates this message is from tool use
+                                    "name": "rag_pipeline",
+                                    "content": str(output),
+                                })
+                        else:
+                            return {
+                                "success": False,
+                                "source": "josaa_rag_tool",
+                                "error": result["error"]
+                            }
+                    except Exception as tool_exec_error:
+                        return {
+                            "success": False,
+                            "source": "josaa_rag_tool",
+                            "error": f"Error during tool execution: {str(tool_exec_error)}"
+                        }
+                elif tool_call.function.name == "web_search":
+                    try:
+                        args = json.loads(tool_call.function.arguments)
+                        result = web_search(**args)
+                        print("web_search tool used")
+                        if result["success"]:
+                            output = result["answer"]
+                            messages.append(
+                                {
+                                    "tool_call_id": tool_call.id,
+                                    "role": "tool",  # Indicates this message is from tool use
+                                    "name": "rag_pipeline",
+                                    "content": str(output),
+                                })
+                        else:
+                            return {
+                                "success": False,
+                                "source": "web_search_tool",
+                                "error": result["error"]
+                            }
+                    except Exception as tool_exec_error:
+                        return {
+                            "success": False,
+                            "source": "web_search_tool",
+                            "error": f"Error during tool execution: {str(tool_exec_error)}"
+                        }
+                elif tool_call.function.name == "find_colleges_in_rank_range":
+                    try:
+                        args = json.loads(tool_call.function.arguments)
+                        result = find_colleges_in_rank_range(**args)
+                        print("ORCR Finder Used")
+                        if result["success"]:
+                            output = result["answer"]
+                            messages.append(
+                                {
+                                    "tool_call_id": tool_call.id,
+                                    "role": "tool",  # Indicates this message is from tool use
+                                    "name": "find_colleges_in_rank_range",
+                                    "content": str(output),
+                                })
+
+                    except Exception as tool_exec_error:
+                        return {
+                            "success": False,
+                            "source": "ORCR Finder Used",
+                            "error": f"Error during tool execution: {str(tool_exec_error)}"
+                        }
+
+
+            try:
+                chat_completion = await get_response(messages= messages, model_name = "meta-llama/llama-4-scout-17b-16e-instruct", tools = None)
+                response = chat_completion.choices[0].message.content
+                return {
+                    "success" : True,
+                    "source" : "agent_llm",
+                    "answer" : str(response),
+                }
+
+            except Exception as e:
+                return {
+                    "success": False,
+                    "source": "agent",
+                    "error": f"Unexpected error in chat_agent: {str(e)}"
+                }
+
+
+
+        else:
+
+            return {
+                "success": True,
+                "source": "agent_llm",
+                "answer": msg.content
+            }
+
+    except Exception as e:
+        return {
+            "success": False,
+            "source": "agent",
+            "error": f"Unexpected error in chat_agent: {str(e)}"
+        }
+
+
+if __name__ == "__main__":
+    chat_agent()
