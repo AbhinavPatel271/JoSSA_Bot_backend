7fbf96891cdedb7a37f2a95186ffdca1b86ee46e
yash <thevoiceofyashbhamare@gmail.com>
2025-05-30 03:33:06 +0400

    added first message output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import asyncio
-from llm_chat_agent import chat_agent
-from reccomender import create_chat_completion
+from llm_chat_agent_v2 import chat_agent
+from agent_tools.ORCR_finder_rank import find_colleges_in_rank_range
 
 
 app = Flask(__name__)
@@ -10,14 +10,18 @@ app = Flask(__name__)
 def first_response_through_DB():
     try:
         data = request.get_json()
+
+        # Obtaining the data from frontend
         category = data.get('category')
         advance_rank = data.get('advance_rank')
         mains_rank = data.get('mains_rank')
-        messages = None
-        create_chat_completion()
+        gender = data.get('gender')
 
+        # Passing the generated prompt through the LLM
+        prompt = f"Can You find me the 10 best colleges based on my rank of {advance_rank}, category {category} and gender {gender}"
+        response = asyncio.run(chat_agent(prompt))["answer"]
 
-
+        # response var is the raw text response for the first query
         # response back to frontend
         
     except Exception as e:
@@ -67,4 +71,13 @@ def further_responses_through_LLM():
 
 
 if __name__ == '__main__':
-    app.run(port=5000 , debug=True)
+    # app.run(port=5000 , debug=True)
+    category = "General"
+    advance_rank = 1698
+    mains_rank = 1869
+    gender = "Male"
+
+    # Passing the generated prompt through the LLM
+    prompt = f"Can You find me the 10 best colleges based on my rank of {advance_rank}, category {category} and gender {gender}"
+    response = asyncio.run(chat_agent(prompt))
+    print(response['answer'])
